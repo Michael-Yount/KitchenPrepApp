9d616e365993a4eae8b6fed35c5fa67284615269
Michaeal Yount <cheshirekatsmile101@gmail.com>
2020-12-06 23:44:12 -0500

    added js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+//load all event listeners
+loadEventListeners();
+
+
 //press area of prep button
 const areaBtn = document.getElementById('areaBtn').addEventListener('submit', (e) => {
     console.log(areaBtn);
@@ -5,10 +9,30 @@ const areaBtn = document.getElementById('areaBtn').addEventListener('submit', (e
 
 // Back button 
 const backBtn = document.getElementById('backBtn').addEventListener('submit', (e) => {
-    
+
 })
 
 //add inventory tags from custom list
 
+//define UI vars 
+const form = document.querySelector('#prep-form');
+const prepList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-preps');
+const filter = document.querySelector('#filter');
+const prepInput = document.querySelector('#prep');
+
+//load all event listeners 
+function loadEventListeners() {
+    //add task event
+    form.addEventListener('submit', addPrep);
+}
+//Add prep
+function addPrep(e) {
+    if(prepInput.value === '') {
+        alert('Add a Prep');
+    }
+
+    e.preventDefault();
+}
 
 
