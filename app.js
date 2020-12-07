aad3240c7584f7d9ee96ad0d42f536825b93ff18
Michaeal Yount <cheshirekatsmile101@gmail.com>
2020-12-06 19:08:00 -0500

    same

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const areaBtn = document.getElementById('areaBtn').addEventListener('submit', (e
     console.log(areaBtn);
 });
 
+// Back button 
+const backBtn = document.getElementById('backBtn').addEventListener('submit', (e) => {
+    
+})
+
 //add inventory tags from custom list
 
 
