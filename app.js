6861caeda6f174375caa9e5a83060762e5d56447
Michaeal Yount <cheshirekatsmile101@gmail.com>
2021-01-26 12:39:39 -0500

    added modal still not working correctly

--- app.js
+++ app.js
@@ -1,2 +1,50 @@
-'use strict';
+"use strict";
 
+// Variables
+
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+//OVERLAY
+
+const overlay = document.getElementById("overlay");
+
+const quickListLinks = document.querySelector(".section-start--link");
+
+// Quick List link BUTTONS
+
+openModalButtons.forEach((link) => {
+  link.addEventListener("click", () => {
+    const modal = document.querySelector(link.dataset.modalTarget);
+      openModal(modal);
+      
+      console.log('open button is working');
+  });
+});
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+
+//close buttons
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal")
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add("active")
+  overlay.classList.add("active")
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove("active")
+  overlay.classList.remove("active")
+}
