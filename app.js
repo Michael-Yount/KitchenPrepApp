c229616051709c3c4bdc72b700ce3a1832ec6bd1
Michaeal Yount <cheshirekatsmile101@gmail.com>
2022-12-26 15:26:57 -0500

    new pizza

--- app.js
+++ app.js
@@ -128,6 +128,7 @@ const timeout = function (s) {
 const showRecipe = async function () {
   try {
     const res = await fetch(
+      "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604691c37cdc054bd034"
       "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
     );
     const data = await res.json();
