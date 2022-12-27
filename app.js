0208f6cb9820e7bbb1f206abdfb8a62c5e6c565a
Michaeal Yount <cheshirekatsmile101@gmail.com>
2022-12-26 21:06:59 -0500

    fix

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ const showRecipe = async function () {
   try {
     const res = await fetch();
     //"https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604691c37cdc054bd034",
-    //"https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
+    ("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
