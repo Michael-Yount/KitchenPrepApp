7f4f6fdbd106530408e1b0e3283c24975457546e
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-01-13 09:21:55 -0500

    new fix

--- app.js
+++ app.js
@@ -129,6 +129,7 @@ const showRecipe = async function () {
   try {
     const res = await fetch(
       //"https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604691c37cdc054bd034",
+      //https://forkify-api.herokuapp.com/api/v2/recipes/:id)
       "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
     );
     const data = await res.json();
@@ -146,7 +147,7 @@ const showRecipe = async function () {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
-    console.log(recipe);
+    console.log(res, data);
   } catch (err) {
     alert(err);
   }
