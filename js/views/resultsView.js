ecab2c74322bbaa21655e706cae9b8da8447a52e
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-01-20 10:35:45 -0500

    done for now

--- js/views/resultsView.js
+++ js/views/resultsView.js
@@ -17,11 +17,7 @@ class ResultsView extends View {
               <div class="preview__data">
                 <h4 class="preview__title">${result.title}</h4>
                 <p class="preview__publisher">${result.publisher}</p>
-                <div class="preview__user-generated">
-                  <svg>
-                    <use href="./referenceLibrary/images/potatoPuffs.png"></use>
-                  </svg>
-                </div>
+                
               </div>
             </a>
           </li>
